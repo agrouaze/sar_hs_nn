a7f090e48a4e0f5e78e71e1a52b02b606c834fb2
grouazel <antoine.grouazel@ifremer.fr>
2024-02-08 15:33:28 +0100

    clean up the package

--- sarhspredictor/__init__.py
+++ sarhspredictor/__init__.py
@@ -1,21 +1,5 @@
-__all__ = (
-    '__version__',
-    'sarhspredictor'
-)
-
-# Or substitute a different method and assign the result to __version__
-import pkg_resources  # part of setuptools
-from pbr.version import VersionInfo
-
-# This one seems to be slower, and with pyinstaller makes the exe a lot bigger
 try:
-    v3 = VersionInfo('sarhspredictor').release_string()
-except:
-    v3='unknown_need_install_via_pip'
-#print('v3 {}'.format(v3))
-#__version__ = pkg_resources.get_distribution("sarhspredictor").version
-__version__ = v3
-
-#import importlib.metadata
-
-#__version__ = importlib.metadata.version('sarhspredictor')
+    from importlib import metadata
+except ImportError: # for Python<3.8
+    import importlib_metadata as metadata
+__version__ = metadata.version('sarhspredictor')
